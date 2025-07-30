f7b3dab626f57b8cc389efc0a5d14ac395727ad3
kritika-4444 <kritikasri444@gmail.com>
2025-07-30 21:03:13 +0530

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA435U5BtcWjxEov9gDgrDOFJGuPfzf8aw",
+  authDomain: "clinic-management-3a6b3.firebaseapp.com",
+  projectId: "clinic-management-3a6b3",
+  storageBucket: "clinic-management-3a6b3.firebasestorage.app",
+  messagingSenderId: "277658465356",
+  appId: "1:277658465356:web:d7bdcd323239e7f5a3b0e9",
+};
+
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
